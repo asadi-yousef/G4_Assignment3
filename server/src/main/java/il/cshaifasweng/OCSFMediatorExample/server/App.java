170adacfd961d04fca4b603c8517a10cbdc4f883
RenataH02 <renata.haiek@gmail.com>
2025-05-24 01:35:02 +0300

    start of database

--- server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/App.java
+++ server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/App.java
@@ -1,18 +1,29 @@
 package il.cshaifasweng.OCSFMediatorExample.server;
 
+import il.cshaifasweng.OCSFMediatorExample.entities.Flower;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import java.io.IOException;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-	
-	private static SimpleServer server;
-    public static void main( String[] args ) throws IOException
-    {
+public class App {
+    private static SimpleServer server;
+
+    public static void main(String[] args) throws IOException {
         server = new SimpleServer(3000);
         server.listen();
+
+        // Test DB logic
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Transaction tx = session.beginTransaction();
+
+            Flower flower = new Flower("Rose", "Red",6, 19.99, null);
+            session.save(flower);
+
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(HibernateUtil::shutdown));
     }
 }
