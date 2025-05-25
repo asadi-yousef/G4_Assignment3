c8062f81d0d2ac1d90856883b951e5114ce00ff8
RenataH02 <renata.haiek@gmail.com>
2025-05-25 13:51:03 +0300

    Merge remote-tracking branch 'origin/Celine1' into Celine1
    # Conflicts:
    #	client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/App.java

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/PrimaryController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/PrimaryController.java
@@ -4,33 +4,21 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import il.cshaifasweng.OCSFMediatorExample.entities.Catalog;
 
 public class PrimaryController {
-	private Catalog catalog;
 
-	public PrimaryController(Catalog catalog) {
-		this.catalog = catalog;
-	}
-	public Catalog getCatalog() {
-		return catalog;
-	}
-	public void setCatalog(Catalog catalog) {
-		this.catalog = catalog;
-	}
-
-    @FXML
-    void sendWarning(ActionEvent event) {
-    	try {
+	@FXML
+	void sendWarning(ActionEvent event) {
+		try {
 			SimpleClient.getClient().sendToServer("#warning");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
+	}
 
 	@FXML
-	void initialize() {
+	void initialize(){
 		try {
 			SimpleClient.getClient().sendToServer("add client");
 		} catch (IOException e) {
