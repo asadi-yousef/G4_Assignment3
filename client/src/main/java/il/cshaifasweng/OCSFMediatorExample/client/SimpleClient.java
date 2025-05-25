ece73d69a6f3a3f497b04f7e6820eac5ed835c53
RenataH02 <renata.haiek@gmail.com>
2025-05-25 13:47:59 +0300

    Merge remote-tracking branch 'origin/Celine1' into Celine1
    # Conflicts:
    #	client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/App.java

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/SimpleClient.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/SimpleClient.java
@@ -7,7 +7,7 @@ import il.cshaifasweng.OCSFMediatorExample.entities.Warning;
 import il.cshaifasweng.OCSFMediatorExample.entities.Catalog;
 
 public class SimpleClient extends AbstractClient {
-	
+
 	private static SimpleClient client = null;
 
 	private SimpleClient(String host, int port) {
@@ -18,7 +18,7 @@ public class SimpleClient extends AbstractClient {
 	protected void handleMessageFromServer(Object msg) {
 		EventBus.getDefault().post(msg);
 	}
-	
+
 	public static SimpleClient getClient() {
 		if (client == null) {
 			client = new SimpleClient("localhost", 3000);
--- server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/App.java
+++ server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/App.java
@@ -30,7 +30,7 @@ public class App {
     }
 
     public static void main(String[] args) throws IOException {
-        server = new SimpleServer(3000);
+        SimpleServer server = new SimpleServer(3000);
         server.listen();
 
         // Test DB logic
